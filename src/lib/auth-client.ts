1f28bda82db99c6b9145b5b7266248eb64bfcc45
smokeyshawn18 <shudarsanpoudel25@gmail.com>
2026-02-15 15:41:32 +0545

    fix auth client issue

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { BACKEND_BASE_URL, USER_ROLES } from "../constants";
 
 export const authClient = createAuthClient({
-  baseURL: `${BACKEND_BASE_URL}auth`,
+  baseURL: `${BACKEND_BASE_URL.replace(/\/$/, "")}/api/auth`,
   user: {
     additionalFields: {
       role: {
